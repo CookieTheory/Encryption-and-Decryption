4d648322844cb79d1f4cc115db034e34b537e89c
CookieTheory <118179323+CookieTheory@users.noreply.github.com>
2022-11-28 08:25:19 +0100

    Removed unnecessary usings

--- Encryption and Decryption/formAES.cs
+++ Encryption and Decryption/formAES.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.IO.Pipes;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Encryption_and_Decryption
 {
